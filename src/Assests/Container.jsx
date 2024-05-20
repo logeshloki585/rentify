fa8fd6416afdc8b159ae675431bfca988354dcec
logeshloki585 <Logeshtv21@gmail.com>
2024-05-20 20:16:58 +0530

    second

--- src/Assests/Container.jsx
+++ src/Assests/Container.jsx
@@ -1,2 +1,2 @@
 // export const END_POINT = "http://localhost:5000";
-export const END_POINT = "https://rentify-backend-omega.vercel.app/";
+export const END_POINT = "https://rentify-backend-omega.vercel.app";
